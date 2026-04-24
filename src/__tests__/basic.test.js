d0b2ff8007ea50b17b470f95127b78c15601c078
Luis Lezama <lezamafortuna@gmail.com>
2026-04-23 21:25:27 -0700

    test file and script added

--- src/__tests__/basic.test.js
+++ src/__tests__/basic.test.js
@@ -1,3 +1,21 @@
 test("math works", () => {
   expect(1 + 1).toBe(2)
 })
+
+test("object assignment", () => {
+  const data = { one: 1 }
+  data["two"] = 2
+  expect(data).toEqual({ one: 1, two: 2 })
+})
+
+test("null", () => {
+  const n = null
+  expect(n).toBeNull()
+})
+
+describe("Basic test", () => {
+  test("adds numbers correctly", () => {
+    const sum = 1 + 2
+    expect(sum).toBe(3)
+  })
+})
